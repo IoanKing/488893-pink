b020a92917490e0d7148c3706cf9cc2ef159fdb9
Ivan Suslov <ioankingisepp@mail.ru>
2018-11-05 21:24:00 +0300

    Устранение ошибок Travis

--- source/js/picturefill.js
+++ source/js/picturefill.js
@@ -1,3 +1,4 @@
+/* stylelint-disable */
 /*! Picturefill - v2.3.1 - 2015-04-09
 * http://scottjehl.github.io/picturefill
 * Copyright (c) 2015 https://github.com/scottjehl/picturefill/blob/master/Authors.txt; Licensed MIT */
